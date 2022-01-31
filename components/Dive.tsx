e7e0f36089054a5ae0f33e926c1f6c45364f34ae
Denis Rajlic <denis.rajlic@gmail.com>
2022-01-31 23:20:23 +0100

    add dive section

--- components/Dive.tsx
+++ components/Dive.tsx
@@ -3,48 +3,59 @@ import Button from './Button';
 import Dive1 from '/public/assets/elements/divein1.svg';
 import Dive2 from '/public/assets/elements/divein2.svg';
 
-const DiveIn = () => (
-  <section className="dive-section page-section inverted-colors element-left-animated element-right-animated">
-    <div className="container">
-      <Dive1 className="svg-el-left"/>
-      <Dive2 className="svg-el-right"/>
-      <div className="row">
-        <div className="col-md-12 col-lg-12 col-xl-10 mx-md-auto">
-          <h2 className="text-center">Dive In The<br/>Creator Pools</h2>
-          <div className="row dive-section-inner">
-            <div className="col-md-6 dive-section-half">
-              <p>
-                <strong>Crypto-powered Creator Economy</strong><br/>
-                Think of Sway Creator Pools as liquidity pools. They both operate on a simple principle of a majority
-                locking in value in the form of a native cryptocurrency in return for future, and to some extent,
-                predictable, rewards.
-                Value is generated through different mechanisms. The most sustainable cases are based on
-                borrowing/lending or fees collected by the AMMs. With Clout.art’s creator pools, value generation is
-                tied directly to the successful sale of an NFT.
-              </p>
+const DiveIn = () => {
+  const text = (
+    <p>
+      <strong>The Sooner, the Better Rewards</strong><br/>
+      To date, creators have minted more than 3500 NFTs, and more than 10,000 users have interacted with the
+      platform. This first-generation staking program is designed to work during this build-up period, thereby
+      offering a first real taste of the new creator economy while we drive platform participation towards
+      critical mass.
+      Start staking with any Instagram or TikTok username that you believe will join the platform in the
+      future. The sooner you start, the better the rewards.
+    </p>
+  );
+
+  return (
+    <section className="dive-section page-section inverted-colors element-left-animated element-right-animated">
+      <div className="container">
+        <Dive1 className="svg-el-left"/>
+        <Dive2 className="svg-el-right"/>
+        <div className="row">
+          <div className="col-md-12 col-lg-12 col-xl-10 mx-md-auto">
+            <h2 className="text-center">Dive In The<br/>Creator Pools</h2>
+            <div className="row dive-section-inner">
+              <div className="col-md-6 dive-section-half">
+                <p>
+                  <strong>Crypto-powered Creator Economy</strong><br/>
+                  Think of Sway Creator Pools as liquidity pools. They both operate on a simple principle of a majority
+                  locking in value in the form of a native cryptocurrency in return for future, and to some extent,
+                  predictable, rewards.
+                  Value is generated through different mechanisms. The most sustainable cases are based on
+                  borrowing/lending or fees collected by the AMMs. With Clout.art’s creator pools, value generation is
+                  tied directly to the successful sale of an NFT.
+                </p>
+              </div>
+              <div className="d-none d-md-block col-md-6 dive-section-half">
+                {text}
+              </div>
             </div>
-            <div className="col-md-6 dive-section-half">
-              <p>
-                <strong>The Sooner, the Better Rewards</strong><br/>
-                To date, creators have minted more than 3500 NFTs, and more than 10,000 users have interacted with the
-                platform. This first-generation staking program is designed to work during this build-up period, thereby
-                offering a first real taste of the new creator economy while we drive platform participation towards
-                critical mass.
-                Start staking with any Instagram or TikTok username that you believe will join the platform in the
-                future. The sooner you start, the better the rewards.
-              </p>
+            <div className="d-block d-md-none row dive-section-inner mt-5">
+              <div className="col-md-6 dive-section-half">
+                {text}
+              </div>
             </div>
+            <Button
+              url="https://github.com/swayprotocol"
+              target="_blank"
+              className="justify-content-center mt-5">
+              Contribute to{' '}<strong>SWAY</strong>
+            </Button>
           </div>
-          <Button
-            url="https://github.com/swayprotocol"
-            target="_blank"
-            className="justify-content-center mt-5">
-            Contribute to{' '}<strong>SWAY</strong>
-          </Button>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+}
 
 export default DiveIn;
